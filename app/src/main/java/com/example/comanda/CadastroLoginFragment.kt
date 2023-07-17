d1f333cb74dbe8f3438156d42e2292839c34c3ad
felip <felipe.dourados00@gmail.com>
2023-07-17 02:41:57 -0300

    cadastro usuario - dataPicker

--- app/src/main/java/com/example/comanda/CadastroLoginFragment.kt
+++ app/src/main/java/com/example/comanda/CadastroLoginFragment.kt
@@ -1,25 +1,25 @@
 package com.example.comanda
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
 import android.widget.Toast
-import android.widget.Toolbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.comanda.databinding.FragmentCadastroLoginBinding
-import kotlin.time.Duration.Companion.seconds
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.Calendar
+import java.util.Calendar.DATE
+import java.util.Calendar.DAY_OF_YEAR
+import java.util.Calendar.YEAR
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 
 
 class CadastroLoginFragment : Fragment() {
@@ -37,24 +37,62 @@ class CadastroLoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.toolbar.setNavigationOnClickListener{
-            findNavController().navigateUp()
+            findNavController().navigate(R.id.action_cadastroLoginFragment_to_login3)
+        }
+        createSelectTipoPessoa()
+
+        binding.editTextDataNascimentoCadastroUsuario.setOnClickListener{
+            showDatePickerDialog()
         }
 
-        val pessoasTypes = listOf("Pessoa Física", "Pessoa Jurídica")
-        val autoComplete = binding.autoCompleteTipoPessoa
-        val adapter = ArrayAdapter(this.requireContext(),R.layout.tipo_pessoa_list_item,pessoasTypes)
-
-        autoComplete.setAdapter(adapter)
-
-        autoComplete.onItemClickListener = AdapterView.OnItemClickListener{
-                adapterView,view,i,l ->
-
-            val itemSelected = adapterView.getItemAtPosition(i)
-            Toast.makeText(this.context,"Item: $itemSelected", Toast.LENGTH_SHORT).show()
-        }
 
     }
 
+    fun createSelectTipoPessoa(){
+        val pessoasTypes = arrayOf("Pessoa Física", "Pessoa Jurídica")
+        val autoComplete = binding.autoCompleteTipoPessoa
+        (autoComplete as? MaterialAutoCompleteTextView)?.setSimpleItems(pessoasTypes)
+
+        /*OUTRA FORMA DE FAZER - USANDO ARRAY ADAPTER
+
+        val adapter = ArrayAdapter(this.requireContext(),R.layout.tipo_pessoa_list_item,pessoasTypes)
+
+        autoComplete.setAdapter(adapter)*/
+
+        //como fazer uma ação apartir da seleção de um item
+        autoComplete.onItemClickListener = AdapterView.OnItemClickListener{
+                adapterView,view,position,l ->
+
+            val itemSelected = adapterView.getItemAtPosition(position)
+            Toast.makeText(this.context,"$itemSelected", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun showDatePickerDialog(){
+
+        val outputDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).apply {
+            timeZone = TimeZone.getTimeZone("UTC-3")
+        }
+        val calendar = Calendar.getInstance()
+
+        val datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .setTextInputFormat(outputDateFormat)
+                .build()
 
 
+
+        datePicker.addOnPositiveButtonClickListener{
+            val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC-3"))
+            calendar.time = Date(it)
+            Toast.makeText(this.context, calendar.time.toString(),Toast.LENGTH_SHORT).show()
+            val month: String
+            if ((calendar.get(Calendar.MONTH) + 1).toString().length < 2) month = "0${calendar.get(Calendar.MONTH) + 1}" else month = "${calendar.get(Calendar.MONTH) + 1}"
+
+            binding.editTextDataNascimentoCadastroUsuario.setText("${calendar.get(Calendar.DAY_OF_MONTH)}/" +
+                    "${month}/${calendar.get(Calendar.YEAR)}")
+        }
+        return datePicker.show(childFragmentManager,"tag")
+    }
 }
---
+++ app/src/main/java/com/example/comanda/DatePickerFragment.kt
@@ -0,0 +1,22 @@
+package com.example.comanda
+
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+class DatePickerFragment : DialogFragment(){
+    /*lateinit var dateListener : DatePickerDialog.OnDateSetListener
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+    }
+
+    companion object{
+        fun newInstance(listener: DatePickerDialog.OnDateSetListener): DatePickerFragment{
+            val instance = DatePickerFragment()
+            instance.s
+        }
+    }*/
+}
